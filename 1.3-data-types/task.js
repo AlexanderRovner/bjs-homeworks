2ab010057f6c74f2ebb53a2d078e7eb67a291602
AlexanderRovner <rovner48@gmail.com>
2021-04-11 18:46:59 +0200

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,49 @@
+'use strict';
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
     // return totalAmount;
+    if(Number(percent)) {
+        percent = Number(percent/100);
+        if(Number(contribution)|| contribution == 0) {
+            contribution = Number(contribution);
+            if(Number(amount)) {
+                amount = Number(amount);
+                if(Number(date)) {
+                    date = Number(date);
+                    let BLOAN = 0, totalAmount = 0;
+                    BLOAN = amount - contribution;
+                    totalAmount = BLOAN*((percent/12)+(percent/12)/((Math.pow((1+(percent/12)),date)) - 1))*date;  
+                    console.log(Number(totalAmount.toFixed(2)));
+                    return Number(totalAmount.toFixed(2));        
+                }
+                else
+                console.log(`Параметр дата окончания кредита содержит
+                 неправильное значение ${date}`);
+            }
+            else
+            console.log(`Параметр сумму кредита содержит
+             неправильное значение ${amount}`);
+        }
+        else
+        console.log(`Параметр сумма первоначального взноса содержит
+         неправильное значение ${contribution}`);
+    }
+    else
+    console.log(`Параметр процентная ставка содержит
+     неправильное значение ${percent}`);
+
+
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     // return greeting;
+    if(name === undefined || name === "") {
+        console.log(`Привет, мир! Меня зовут Аноним`);
+        return "Привет, мир! Меня зовут Аноним.";
+    }
+    else {
+        console.log(`Привет, мир! Меня зовут ${name}`);
+        return "Привет, мир! Меня зовут " + name+ ".";
+    }
 }
