4413b2dee36f2132e397b9cded259caa48374dcf
AlexanderRovner <rovner48@gmail.com>
2021-04-18 11:39:12 +0200

    Update task.js

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,5 +1,5 @@
 //String.prototype.isPalindrome - для задачи №1
-
+'use strict'
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
     // return averageMark
@@ -9,3 +9,18 @@ function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
     // return verdict
 }
+
+
+Object.defineProperty(String.prototype, 'isPalindrome', {
+    value: function isPalindrome() {
+        let str = this;
+        str = str.toLowerCase().replace(/\s/g, '');
+        for (let i = 0; i <= str.length / 2; i++) {
+            if (str[i] !== str[str.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+});
