bc60d64877773e5b32a457d29dd7fd85056e88f7
AlexanderRovner <rovner48@gmail.com>
2021-04-19 23:41:16 +0200

    Update task.js

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -100,18 +100,15 @@ class Library {
                 return bookName;
             }
         }
-        
         return null;
     }
 }
 
 
 const library = new Library("Библиотека имени Ленина");
-library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
 library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
 library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
 library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
 library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
-library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
-library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+
 
