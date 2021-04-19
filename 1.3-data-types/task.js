ec9dfdec5e8333c58fba2fb86080acd9fca88d3d
AlexanderRovner <rovner48@gmail.com>
2021-04-19 10:58:47 +0200

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -5,27 +5,27 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     // console.log(percent, contribution, amount, date);
     // console.log(typeof(percent),typeof(contribution),typeof(amount),typeof date);
     // console.log(date.getTime());
-    let timeDelta;
     let dateToday = new Date();
-    if (percent === undefined || percent <= 0 || isNaN(percent)) {
+    let timeDelta = date.getTime() - dateToday.getTime();
+    if (isNaN(percent) || percent <= 0) {
         return `Параметр процентная ставка содержит
      неправильное значение ${percent}`;
     }
-    if (contribution === undefined || contribution < 0 || isNaN(contribution)) {
+    if (isNaN(contribution)|| contribution < 0) {
         return `Параметр процентная ставка содержит
         неправильное значение ${contribution}`;
     }
-    if (amount === undefined || amount <= 0 || isNaN(amount)) {
+    if ( isNaN(contribution) || amount <= 0) {
         return `Параметр сумму кредита содержит
             неправильное значение ${amount}`;
     }
-    if (date === undefined || date <= 0 || (timeDelta = date.getTime() - dateToday.getTime()) <= 0) {
+    if (date === undefined || date <= 0 || timeDelta <= 0) {
         return `Параметр дата окончания кредита содержит
                 неправильное значение ${date}`;
     }
     let BLOAN = amount - contribution;
     let months = Math.floor(((timeDelta) / (1000 * 60 * 60 * 24 * 365)) * 12);
-    percent = percent / 100 * 12;
+    percent = percent / (100 * 12);
     let totalAmount = BLOAN * ((percent) + (percent) / ((Math.pow((1 + (percent)), months)) - 1)) * months;
     console.log(Number(totalAmount.toFixed(2)));
     return Number(totalAmount.toFixed(2));
